49a517f0ad9a0b273376daae8aaba4ece5da9815
mKMariano <mikahilmariano@gmail.com>
2025-09-02 01:59:21 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Cautoo',
+  description: 'Assistência veicular',
+  generator: 'Cautoo',
 }
 
 export default function RootLayout({
